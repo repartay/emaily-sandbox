b32327d82a3f0e3bf25f3d7e220811c1924ecc3a
Margaret James <mpinaforejames@gmail.com>
2018-02-19 17:46:49 -0700

    almost done w/ oauth

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
 
-app.get('/', (req, res) => {
-	res.send({ hi: 'there' });
-});
+// generated from Google API dev console
+passport.use(
+	new GoogleStrategy(
+		{
+			clientID: keys.googleClientID,
+			clientSecret: keys.googleClientSecret,
+			callbackURL: '/auth/google/callback'
+		},
+		accessToken => {
+			console.log(accessToken);
+		}
+	)
+);
+
+// route handler for oauth pt1, that puts user into passport flow
+app.get(
+	'/auth/google', 
+	passport.authenticate('google', {
+		scope: ['profile', 'email']
+	})
+);
+// route handler for oauth pt2, exchanges code for user profile info
+app.get(
+	'/auth/google/callback',
+	passport.authenticate('google')
+);
 
 // see if underlying env (heroku) has declared what port to use
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT);
