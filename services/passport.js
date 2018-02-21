b96e98ef880639331977c7a9c5dbc8dcc595ff70
Margaret James <mpinaforejames@gmail.com>
2018-02-21 12:25:32 -0700

    add serialize and deserialize from passport

--- services/passport.js
+++ services/passport.js
@@ -5,6 +5,18 @@ const keys = require('../config/keys');
 
 const User = mongoose.model('users');
 
+// user = mongoose model instance. turned it into id
+passport.serializeUser((user, done) => {
+	done(null, user.id);
+});
+
+// turning an id into a mongoose model instance
+passport.deserializeUser((id, done) => {
+	User.findById(id).then(user => {
+		done(null, user);
+	});
+});
+
 passport.use(
 	new GoogleStrategy(
 		{
@@ -17,12 +29,14 @@ passport.use(
 			.then((existingUser) => {
 				if (existingUser){
 					// we already have a rec w/ given profile.id
-					
+					done(null, existingUser);
 				} else {
-					// we dont have a user record w/ this profile.id, make new record
-					new User({ googleId: profile.id }).save();
+					// we dont have a user record w/ this profile.id, make new record (async)
+					new User({ googleId: profile.id })
+					.save()
+					.then(user => done(null, user));
 				}
-			})
+			});
 			
 		}
 	)
