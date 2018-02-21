d2f0283a8c1a180422510d6b62b3d4bb0af3d20d
Margaret James <mpinaforejames@gmail.com>
2018-02-21 13:15:56 -0700

    add ability to logout

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -7,10 +7,16 @@ module.exports = (app) => {
 			scope: ['profile', 'email']
 		})
 	);
-
 	app.get(
 		'/auth/google/callback',
 		passport.authenticate('google')
+	);
+
+	app.get('/api/logout', 
+		(req, res) => {
+			req.logout();
+			res.send(req.user);
+		}
 	);
 
 	app.get(
