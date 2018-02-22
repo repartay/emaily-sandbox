b420e06160bc83dd6950a1a404539d9f366f4417
Margaret James <mpinaforejames@gmail.com>
2018-02-22 14:15:43 -0700

    header logo conditional based on login auth status

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { BrowserRouter, Route } from 'react-router-dom';
 import * as actions from '../actions';
-
 import  Header from './Header';
+import Landing from './Landing';
+
 const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
-const Landing = () => <h2>Landing</h2>;
 
 class App extends Component {
 	componentDidMount() {
