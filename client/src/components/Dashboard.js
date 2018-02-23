cea6971aa0f9adcbaff31c4584a4b40f4545bd47
Margaret James <mpinaforejames@gmail.com>
2018-02-23 16:12:37 -0700

    add dash page and materialize css icons to index

---
+++ client/src/components/Dashboard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Dashboard = () => {
+	return (
+		<div>
+			<h1>Dash!</h1>
+			<div className="fixed-action-btn">
+				<a className="btn-floating btn-large red">
+					<i className="material-icons">add</i>
+				</a>
+			</div>
+		</div>
+	);
+};
+
+export default Dashboard;
