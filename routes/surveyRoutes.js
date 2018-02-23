a7481d1fc25a680210c24e6801f264fda5a73840
Margaret James <mpinaforejames@gmail.com>
2018-02-23 12:22:11 -0700

    WIP wiring up POST req for survey route handler

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -7,10 +7,13 @@
 // 1. user is logged in
 const requireLogin = require('../middlewares/requireLogin');
 // 2. user has enough credits
-
+const requireCredits = require('../middlewares/requireCredits');
 
 module.exports = app => {
-	app.post('/api/surveys', requireLogin, (req, res) => {
-		console.log('req', req, res);
+	app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+		// 1. req should contain: title, subject, body, recipients
+		const { title, subject, body, recipients } = req.body;
+		// 2. use them to create a new instance of mongo survey
+		
 	});
 };
