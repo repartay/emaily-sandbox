ae69c4a6c9e6eb8c096dc8c59eca214600ded822
Margaret James <mpinaforejames@gmail.com>
2018-02-23 15:19:04 -0700

    sendgrid failing, so saving to survey collection w/o webhook

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,8 +1,8 @@
-const mongoose = require('mongoose');
 // Routes HOW TO:
 // define arrow function
 // immediately export it
 // wire it up to express app (into index.js)
+const mongoose = require('mongoose');
 
 // before executing, we want to make sure:
 // 1. user is logged in
@@ -16,9 +16,10 @@ const Survey = mongoose.model('surveys');
 
 // Creating a new survey and send out a big email
 module.exports = app => {
-	app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+	app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
 		// 1. req should contain: title, subject, body, recipients
 		const { title, subject, body, recipients } = req.body;
+		// console.log('req.body', req.body);
 		// 2. use them to create a new instance of mongo survey IN MEMORY
 		const survey = new Survey({
 			title,
@@ -29,12 +30,18 @@ module.exports = app => {
 			dateSent: Date.now()
 		});
 		// 3a. Send the email. 2nd argument will be template
-		const mailer = new Mailer(survey, surveyTemplate(survey));
-
+		// const mailer = new Mailer(survey, surveyTemplate(survey));
+		// console.log('mailer=====', mailer);
 		// 3b. did it send successfully?
-
+		try {
+			// await mailer.send();
+			await survey.save();
+			req.user.credits -= 1;
+			const user = await req.user.save();
 		// 4. if YES, call save on new instance. if NO, alert message
-		// 5. route handler complete :-)
-
+			res.send(user);
+		} catch (err) {
+			res.status(422).send(err);
+		}
 	});
 };
