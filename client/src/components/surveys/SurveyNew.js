93390245447b2a178b0799f57d76f2fcd3af75f1
Margaret James <mpinaforejames@gmail.com>
2018-02-23 16:36:16 -0700

    add redux forms and wire up init components

---
+++ client/src/components/surveys/SurveyNew.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import SurveyForm from './SurveyForm';
+
+class SurveyNew extends Component {
+	render() {
+		return (
+			<div>
+				New Survey Form
+				<SurveyForm />
+			</div>
+		);
+	}
+};
+
+export default SurveyNew;
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
+import { reducer as ReduxForm } from 'redux-form';
 import authReducer from './authReducer';
 
 export default combineReducers({
-	auth: authReducer
+	auth: authReducer,
+	form: ReduxForm
 });
