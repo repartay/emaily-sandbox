89d19c967a991ee4d3eb420ac4cc8ab05e20c736
Margaret James <mpinaforejames@gmail.com>
2018-02-23 17:22:03 -0700

    logging input of single field from survey

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
+import { reduxForm, Field } from 'redux-form';
 
 class SurveyForm extends Component {
 	render() {
 		return (
 			<div>
-				Survey Form
+				<form onSubmit={this.props.handleSubmit(values => console.log('values', values))}>
+					<h2>Survey Form</h2>
+					<div>
+						<label>Survey Title</label>
+						<Field
+							type="text"
+							name="surveyTitle"
+							component="input"
+						/>
+					</div>
+					<button className="btn waves-effect waves-light" type="submit">
+						<i className="material-icons right">send</i>
+          				Submit
+        			</button>
+				</form>
 			</div>
 		);
 	}
 };
 
-export default SurveyForm;
+export default reduxForm({
+	form: 'surveyForm'
+})(SurveyForm);
