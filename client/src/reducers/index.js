09f2a18cedb6a382998593b4d7d1c5254be762b3
Margaret James <mpinaforejames@gmail.com>
2018-02-25 15:56:17 -0700

    front end pt2 write reducers

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { reducer as ReduxForm } from 'redux-form';
 import authReducer from './authReducer';
+import surveysReducer from './surveysReducer';
 
 export default combineReducers({
 	auth: authReducer,
-	form: ReduxForm
+	form: ReduxForm,
+	surveys: surveysReducer
 });
