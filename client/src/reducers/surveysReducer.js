19e3a434de4254ff0c09ab2aadb00c667a1cd228
Margaret James <mpinaforejames@gmail.com>
2018-02-25 15:56:53 -0700

    fix typo

--- client/src/reducers/surveysReducer.js
+++ client/src/reducers/surveysReducer.js
@@ -3,7 +3,7 @@ import { FETCH_SURVEYS } from '../actions/types';
 export default function(state = [], action) {
 	switch(action.type) {
 		case FETCH_SURVEYS:
-			return action.paylaod;
+			return action.payload;
 		default:
 			return state;
 	}
