13682f0691bd6210f1ff4d4b2f911c07cea8944f
Margaret James <mpinaforejames@gmail.com>
2018-02-25 16:11:04 -0700

    surveys appearing nicely in survey list

--- client/src/components/surveys/SurveyList.js
+++ client/src/components/surveys/SurveyList.js
@@ -6,9 +6,28 @@ class SurveyList extends Component {
 	componentDidMount() {
 		this.props.fetchSurveys();
 	}
+	renderSurveys() {
+		return this.props.surveys.map(survey => {
+			return (
+				<div className="card darken-1" key={survey._id}>
+					<div className="card-content">
+						<span className="card-title">{survey.title}</span>
+						<p>
+							{survey.body}
+						</p>
+						<p className="right">
+							Sent On: {new Date(survey.dateSent).toLocaleDateString()}
+						</p>
+					</div>
+          		</div>
+			);
+		})
+	}
 	render() {
 		return (
-			<div>SurveyList</div>
+			<div>
+			{this.renderSurveys()}
+			</div>
 		);
 	}
 }
